9b82c4518af1477100f621f9f3a97ad78f623ce4
Miguel A. Segovia Freeman. <miguelsegoviafreeman@gmail.com>
2022-10-25 18:21:35 +0200

    Añadido para hacer procesos en java.

---
+++ PSP-Java/src/Ejemplo1.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Ejemplo1 {
+   public static void main(String[] args) throws IOException  {	   
+	   ProcessBuilder pb = new ProcessBuilder("NOTEPAD");
+	   Process p = pb.start();
+   }
+}//Ejemplo1
