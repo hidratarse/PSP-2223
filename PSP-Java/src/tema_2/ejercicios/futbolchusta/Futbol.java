c6affd39bf8b2f919279601b9bc286b05892dde6
Miguel A. Segovia Freeman. <miguelsegoviafreeman@gmail.com>
2022-11-08 20:13:45 +0100

    Falta terminar el furbol

--- PSP-Java/src/tema_2/ejercicios/futbolchusta/Futbol.java
+++ PSP-Java/src/tema_2/ejercicios/futbolchusta/Futbol.java
@@ -1,11 +1,40 @@
 package tema_2.ejercicios.futbolchusta;
 
+import java.time.LocalTime;
+
+import javax.swing.JLabel;
+
 public class Futbol implements Runnable{
+	
+	JLabel _salida;
+	int cont = 0;
+	
+	public Futbol(JLabel salida) {
+		_salida=salida;
+	}
+	
+	public String firstTwo(String str) {
+	    return str.length() < 2 ? str : str.substring(0, 2);
+	}
 
 	@Override
 	public void run() {
+		
+		String hora;
+		String minuto;
+		String segundos;
+		String nano;
+		
 		while (true) {
+			LocalTime tiempo = java.time.LocalTime.now();
+			hora = Integer.toString(tiempo.getHour());
+			minuto = Integer.toString(tiempo.getMinute());
+			segundos = Integer.toString(tiempo.getSecond());
+			nano = Integer.toString(tiempo.getNano());
 			
+			nano = firstTwo(nano);
+			
+			_salida.setText(hora+":"+minuto+":"+segundos+":"+nano);
 		}
 	}
 
--- PSP-Java/src/tema_2/ejercicios/futbolchusta/Juego.java
+++ PSP-Java/src/tema_2/ejercicios/futbolchusta/Juego.java
@@ -1,5 +1,43 @@
 package tema_2.ejercicios.futbolchusta;
 
-public class Juego {
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Juego extends JFrame{
+	private static final long serialVersionUID = 4250627773234114346L;
+	
+	public Juego() {
+		super("Juego");
+		JLabel l = new JLabel();
+		l.setText("Hola mundo");
+		setLayout(new FlowLayout());
+		getContentPane().add(l);
+		
+		JButton b = new JButton("pulsa");
+		
+		Futbol c=new Futbol(l);
+		
+		b.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Thread t =new Thread(c);
+				t.start();
+			}
+		});
+		getContentPane().add(b);
+		setSize(500,400);
+		setVisible(true);
+		
+	}
+	
+	public static void main(String[] args) {
+		new Juego();
+	}
 
 }
