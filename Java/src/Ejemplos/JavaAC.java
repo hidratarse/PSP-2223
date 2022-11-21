f3639987b10fbaa98159daefe0933472e9f6a31c
Miguel Ángel desde clase <miguelsegoviafreeman@gmail.com>
2022-11-21 18:25:27 +0100

    JAVA A C COMPLETADO, PULIR

--- Java/src/Ejemplos/JavaAC.java
+++ Java/src/Ejemplos/JavaAC.java
@@ -3,21 +3,21 @@ package Ejemplos;
 import java.io.InputStream;
 
 public class JavaAC {
-	 public static void main(String[] args) {
-		    try {
-		        Process p = new ProcessBuilder("/home/sergiotap/Documentos/PSP/repaso/ejercicioC","ejercicioC").start();
-		        InputStream is=p.getInputStream();
-		        int c;
-		        System.out.println("Antes del bucle");
-		        System.out.println();
-		        while((c=is.read())!=0)
-		            System.out.print((char)c);
-		        is.close();
-		    } catch (Exception e) {
-		        e.printStackTrace();
-		    }	   
-			   
-		   }
+    public static void main(String[] args) {
+           try {
+               Process p = new ProcessBuilder("/home/miguel/PSP/PSP-2223/C/Ejercicios/pipeSeñal","pipeSeñal").start();
+               InputStream is=p.getInputStream();
+               int c;
+               System.out.println("Antes del bucle");
+               System.out.println();
+               while((c=is.read())!=-1)
+                   System.out.print((char)c);
+               is.close();
+           } catch (Exception e) {
+               e.printStackTrace();
+           }	   
+              
+          }
 }
 
 //Trabajar sobre esto
