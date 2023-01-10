21e61827041af9b1e8c6db390d1758e8835c3899
Miguel <miguelsegoviafreeman@gmail.com>
2023-01-10 20:02:03 +0100

    mondongo

---
+++ Java/EV2/protocolos/src/App.java
@@ -0,0 +1,20 @@
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            Socket con = new Socket("192.168.20.213", 1234);
+            BufferedOutputStream out = new BufferedOutputStream(con.getOutputStream());
+            out.write("mondongo".getBytes());
+            out.close();
+            con.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
